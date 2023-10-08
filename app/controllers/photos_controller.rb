877da264b2a49022055f5ba4d5c10bf8b7aa6ed6
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-10-08 23:20:13 +0000

    finished delete and create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,33 @@ class PhotosController < ApplicationController
     @list_of_photos = matching_photos.order({ :created_at => :desc })
     render({ :template => "photo_templates/index"})
   end
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id})
+    @the_photo = matching_photos.at(0)
+    render({ :template => "photo_templates/show"})
+  end
+  def destroy
+    #Parameters: {"path_id"=>"880"}
+    the_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => the_id})
+    the_photo = matching_photos.at(0)
+    the_photo.destroy 
+    #render({ :template => "photo_templates/destroy"})
+    redirect_to("/photos")
+  end 
+  def create
+    #Parameters: {"input_image"=>"a", "input_caption"=>"b", "input_owner_id"=>"112"}
+    input_image = params.fetch("input_image")
+    input_caption = params.fetch("input_caption")
+    input_owner = params.fetch("input_owner_id")
+
+    input_photo = Photo.new
+    input_photo.image = input_image
+    input_photo.caption = input_caption
+    input_photo.owner_id = input_owner
+    input_photo.save
+    #render({ :template => "photo_templates/create"})
+    redirect_to("/photos/" + input_photo.id.to_s)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get("/", {:controller => "users", :action => "index"})
+
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:path_username", {:controller => "users", :action => "show"})
+
   get("/photos", { :controller => "photos", :action => "index" })
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
+  post("/create_photo", { :controller => "photos", :action => "create"})
 end
