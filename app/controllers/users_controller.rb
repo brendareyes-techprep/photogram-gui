d3dccb8ff702259cb0e7d22d966801cafc2c170e
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-10-08 19:54:37 +0000

    fixed show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
   def index
-    matching_user = User.all.order({:username => :asc})
+    matching_users = User.all
+    @list_of_users = matching_users.order({ :username => :asc })
     render({ :template => "user_templates/index"})
   end 
+  def show
+  url_username = params.fetch("path_username")
+  matching_usernames = User.where({ :username => url_username})
+  @the_user = matching_usernames.at(0)
+  #if the_user == nil
+    #redirect_to("/404")
+  #else
+    render({ :template => "user_templates/show"})
+  #end
+  end
 end
